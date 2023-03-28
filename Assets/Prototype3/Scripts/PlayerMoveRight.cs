8a8a1fbd24852c5e420373e97706e3b0e447b6fc
amer-palo <amer.190501@hotmail.com>
2023-03-28 15:48:08 +1100

    Prototype 3 Work

---
+++ Assets/Prototype3/Scripts/PlayerMoveRight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMoveRight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool isPressed = false;
+    public GameObject player;
+    public float force;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            //player.transform.Translate(force * Time.deltaTime, 0, 0);
+            player.transform.rotation = Quaternion.Euler(0, 0, -45);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+}
