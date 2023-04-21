fb98dbd7431b075b9eb680091c966abcb9a1b6ad
amer-palo <amer.190501@hotmail.com>
2023-04-22 07:13:51 +1000

    UI

--- Assets/Prototype4/Scripts/ScoreCounter.cs
+++ Assets/Prototype4/Scripts/ScoreCounter.cs
@@ -10,9 +10,22 @@ public class ScoreCounter : MonoBehaviour
     public static int correctAnswer;
     public static int incorrectAnswer;
 
+    public GameObject winUI;
+    public GameObject loseUI;
+
     void Update()
     {
         correctText.GetComponent<TextMeshProUGUI>().text = "Answers Correct: " + correctAnswer + "/20";
         incorrectText.GetComponent<TextMeshProUGUI>().text = "Answers Incorrect: " + incorrectAnswer + "/5";
+
+        if(correctAnswer == 20)
+        {
+            winUI.SetActive(true);
+        }
+
+        if(incorrectAnswer == 5)
+        {
+            loseUI.SetActive(true);
+        }
     }
 }
